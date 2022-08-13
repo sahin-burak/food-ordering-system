13b8916d5f1c4bf24bae710401ddfc114b93b4ca
Lucas Barbat <llucbb.mac@gmail.com>
2022-08-13 19:25:07 +0200

    Kafka producer

---
+++ infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/exception/KafkaProducerException.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.kafka.producer.exception;
+
+public class KafkaProducerException extends RuntimeException {
+
+  public KafkaProducerException(String message) {
+    super(message);
+  }
+
+  public KafkaProducerException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
