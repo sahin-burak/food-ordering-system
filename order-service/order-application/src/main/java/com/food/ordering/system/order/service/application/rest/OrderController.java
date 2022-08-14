d48ecf2f63c27178cbb8daf889a2f682956e5d50
Lucas Barbat <llucbb.mac@gmail.com>
2022-08-14 10:15:14 +0200

    Rest controller

---
+++ order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/rest/OrderController.java
@@ -0,0 +1,52 @@
+package com.food.ordering.system.order.service.application.rest;
+
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import com.food.ordering.system.order.service.domain.ports.input.service.OrderApplicationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/order", produces = "application/vnd.api.v1+json")
+@RequiredArgsConstructor
+public class OrderController {
+
+  private final OrderApplicationService orderApplicationService;
+
+  @PostMapping
+  public CreateOrderResponse createOrder(@RequestBody CreateOrderCommand createOrderCommand) {
+
+    log.info(
+        "Creating order for customer: {} at restaurant: {}",
+        createOrderCommand.getCustomerId(),
+        createOrderCommand.getRestaurantId());
+
+    CreateOrderResponse createOrderResponse =
+        orderApplicationService.createOrder(createOrderCommand);
+
+    log.info("Order created with tracking id: {}", createOrderResponse.getOrderTrackingId());
+
+    return createOrderResponse;
+  }
+
+  @GetMapping("/{trackingId}")
+  public TrackOrderResponse getOrderByTrackingId(@PathVariable UUID trackingId) {
+    TrackOrderResponse trackOrderResponse =
+        orderApplicationService.trackOrder(
+            TrackOrderQuery.builder().orderTrackingId(trackingId).build());
+    log.info(
+        "Returning order status with tracking id: {}", trackOrderResponse.getOrderTrackingId());
+    return trackOrderResponse;
+  }
+}
