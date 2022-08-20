3204191c26adaeaa9216f5e4e65fc7c072ab023d
Lucas Barbat <llucbb.mac@gmail.com>
2022-08-20 18:47:05 +0200

    Message publishers

---
+++ order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/mapper/OrderMessagingDataMapper.java
@@ -0,0 +1,71 @@
+package com.food.ordering.system.order.service.messaging.mapper;
+
+import com.food.ordering.system.kafka.avro.model.PaymentOrderStatus;
+import com.food.ordering.system.kafka.avro.model.PaymentRequestAvroModel;
+import com.food.ordering.system.kafka.avro.model.Product;
+import com.food.ordering.system.kafka.avro.model.RestaurantApprovalRequestAvroModel;
+import com.food.ordering.system.kafka.avro.model.RestaurantOrderStatus;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
+import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderMessagingDataMapper {
+
+  public PaymentRequestAvroModel orderCreatedEventToPaymentRequestAvroModel(
+      OrderCreatedEvent orderCreatedEvent) {
+    Order order = orderCreatedEvent.getOrder();
+    return PaymentRequestAvroModel.newBuilder()
+        .setId(UUID.randomUUID().toString())
+        .setSagaId("")
+        .setCustomerId(order.getCustomerId().getValue().toString())
+        .setOrderId(order.getId().getValue().toString())
+        .setPrice(order.getPrice().getAmount())
+        .setCreatedAt(orderCreatedEvent.getCreatedAt().toInstant())
+        .setPaymentOrderStatus(PaymentOrderStatus.PENDING)
+        .build();
+  }
+
+  public PaymentRequestAvroModel orderCancelledEventToPaymentRequestAvroModel(
+      OrderCancelledEvent orderCancelledEvent) {
+    Order order = orderCancelledEvent.getOrder();
+    return PaymentRequestAvroModel.newBuilder()
+        .setId(UUID.randomUUID().toString())
+        .setSagaId("")
+        .setCustomerId(order.getCustomerId().getValue().toString())
+        .setOrderId(order.getId().getValue().toString())
+        .setPrice(order.getPrice().getAmount())
+        .setCreatedAt(orderCancelledEvent.getCreatedAt().toInstant())
+        .setPaymentOrderStatus(PaymentOrderStatus.CANCELLED)
+        .build();
+  }
+
+  public RestaurantApprovalRequestAvroModel orderPaidEventRestaurantApprovalRequestAvroModel(
+      OrderPaidEvent orderPaidEvent) {
+    Order order = orderPaidEvent.getOrder();
+    return RestaurantApprovalRequestAvroModel.newBuilder()
+        .setId(UUID.randomUUID().toString())
+        .setSagaId("")
+        .setOrderId(order.getId().getValue().toString())
+        .setRestaurantId(order.getRestaurantId().getValue().toString())
+        .setOrderId(order.getId().getValue().toString())
+        .setRestaurantOrderStatus(RestaurantOrderStatus.valueOf(order.getOrderStatus().name()))
+        .setProducts(
+            order.getItems().stream()
+                .map(
+                    orderItem ->
+                        Product.newBuilder()
+                            .setId(orderItem.getProduct().getId().getValue().toString())
+                            .setQuantity(orderItem.getQuantity())
+                            .build())
+                .collect(Collectors.toList()))
+        .setPrice(order.getPrice().getAmount())
+        .setCreatedAt(orderPaidEvent.getCreatedAt().toInstant())
+        .setRestaurantOrderStatus(RestaurantOrderStatus.PAID)
+        .build();
+  }
+}
---
+++ order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/publisher/kafka/PayOrderKafkaMessagePublisher.java
@@ -0,0 +1,50 @@
+package com.food.ordering.system.order.service.messaging.publisher.kafka;
+
+import com.food.ordering.system.kafka.avro.model.RestaurantApprovalRequestAvroModel;
+import com.food.ordering.system.kafka.consumer.service.KafkaProducer;
+import com.food.ordering.system.order.service.domain.config.OrderServiceConfigData;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
+import com.food.ordering.system.order.service.domain.ports.output.message.publisher.restaurantapproval.OrderPaidRestaurantRequestMessagePublisher;
+import com.food.ordering.system.order.service.messaging.mapper.OrderMessagingDataMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PayOrderKafkaMessagePublisher implements OrderPaidRestaurantRequestMessagePublisher {
+
+  private final OrderMessagingDataMapper orderMessagingDataMapper;
+  private final OrderServiceConfigData orderServiceConfigData;
+  private final KafkaProducer<String, RestaurantApprovalRequestAvroModel> kafkaProducer;
+  private final OrderKafkaMessageHelper orderKafkaMessageHelper;
+
+  @Override
+  public void publish(OrderPaidEvent domainEvent) {
+    String orderId = domainEvent.getOrder().getId().getValue().toString();
+
+    try {
+      RestaurantApprovalRequestAvroModel restaurantApprovalRequestAvroModel =
+          orderMessagingDataMapper.orderPaidEventRestaurantApprovalRequestAvroModel(domainEvent);
+
+      kafkaProducer.send(
+          orderServiceConfigData.getRestaurantApprovalRequestTopicName(),
+          orderId,
+          restaurantApprovalRequestAvroModel,
+          orderKafkaMessageHelper.getKafkaCallback(
+              orderServiceConfigData.getRestaurantApprovalResponseTopicName(),
+              restaurantApprovalRequestAvroModel,
+              orderId,
+              "RestaurantApprovalRequestAvroModel"));
+
+      log.info("RestaurantApprovalRequestAvroModel sent to kafka for order id: {}", orderId);
+
+    } catch (Exception e) {
+      log.error(
+          "Error while sending RestaurantApprovalRequestAvroModel message to kafka with order id: {}, error: {}",
+          orderId,
+          e.getMessage());
+    }
+  }
+}
